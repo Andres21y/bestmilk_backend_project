567c053766298441c608d1232bce36160e45553d
Andres21y <andres17vh@gmail.com>
2026-03-11 16:04:27 -0500

    Refactor: The field nombre in model breed is renamed to name to maintain consistency.

--- models/Breed.model.js
+++ models/Breed.model.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
 const breedSchema = new mongoose.Schema({
-    nombre: {
+    name: {
         type: String,
         required: true,
         unique: true,
         trim: true
     },
-    descripcion: { type: String }
+    description: { type: String }
 }, { timestamps: true });
 
 export default mongoose.model('Breed', breedSchema);
